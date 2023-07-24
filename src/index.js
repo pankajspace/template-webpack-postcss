68286ac5807b95e2b20abe6d1d5e5ca83bb4fc99
Pankaj Wakchaure <pankaj.corp@gmail.com>
2023-07-24 11:07:56 +0530

    CSS Modules setup done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./index.css";
-// import css from "./index.css";
+import homeStyles from "./home.module.css";
+import aboutStyles from "./about.module.css";
 import webpackLogo from "./img/webpack.png";
 
 console.log('Hello Webpack');
@@ -7,3 +8,10 @@ console.log('Hello Webpack');
 $(".hello").html("Hello Webpack");
 
 $("#image").attr("src", webpackLogo);
+
+$("#css-modules-1").addClass(homeStyles.color);
+$("#css-modules-2").addClass(aboutStyles.color);
+
+$("#image").addClass(homeStyles.image);
+
+
